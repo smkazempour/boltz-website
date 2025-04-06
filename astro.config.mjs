1262ef1cd9c0144024da105060ebcd2df226f71e
smkazempour <107328753+smkazempour@users.noreply.github.com>
2025-04-06 01:07:58 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://smkazempour.github.io',
-  base: '/boltz-website',
+  site: 'https://smkazempour.com',
+  base: '/',
   outDir: 'docs',
   integrations: [tailwind()]
 });
